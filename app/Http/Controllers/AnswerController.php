4f49d8f26dfb8d8bea24106534145f1368928d7b
Carlos Jaldin <carjal192000@gmail.com>
2021-11-22 01:44:37 -0400

    tested and ready

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -28,64 +28,40 @@ class AnswerController extends Controller
        $answers = Answer::where('quiz_id', $id)->paginate(5);
        $answers->load('question');
        $pregun = Answer::select('id')->where('quiz_id', $id)->paginate(5);
-       $v1 = 0;
-       $v2 = 0;
-       $v3 = 0;
-       $v4 = 0;
+
        $p1 = DB::table('answers')
-       ->whereIn('id', $pregun)
+       ->where('quiz_id', $id)
        ->whereIn('question_id', Question::select('id')->where('career_id',1))
        ->sum('value');
        $p2 = DB::table('answers')
-       ->whereIn('id', $pregun)
+       ->where('quiz_id', $id)
        ->whereIn('question_id', Question::select('id')->where('career_id',2))
        ->sum('value');
        $p3 = DB::table('answers')
-       ->whereIn('id', $pregun)
+       ->where('quiz_id', $id)
        ->whereIn('question_id', Question::select('id')->where('career_id',3))
        ->sum('value');
        $p4 = DB::table('answers')
-       ->whereIn('id', $pregun)
+       ->where('quiz_id', $id)
        ->whereIn('question_id', Question::select('id')->where('career_id',4))
        ->sum('value');
-       
-       foreach ($answers as $answer){
-        
-            if($answer->question->career_id == 1){
-                //$v1 = ($v1 == -1000)? 0: $v1;
-                $v1+= $answer->value;
-            }
-            if($answer->question->career_id == 2){
-                //$v2 = ($v2 == -1000)? 0: $v2;
-                $v2+= $answer->value;
-            }
-            if($answer->question->career_id == 3){
-                //$v3 = ($v3 == -1000)? 0: $v3;
-                $v3+= $answer->value;
-            }
-            if($answer->question->career_id == 4){
-                //$v4 = ($v4 == -1000)? 0: $v4;
-                $v4+= $answer->value;
-            }
-            
-       }
-       //dd($v2);
+
        $max = max($p1,$p2,$p3,$p4);
        
        $career = 0;
-       if($max == $v1){
+       if($max == $p1){
         $career = 1;
        }
-       if($max == $v2){
+       if($max == $p2){
         $career = 2;
        }
-       if($max == $v3){
+       if($max == $p3){
         $career = 3;
        }
-       if($max == $v4){
+       if($max == $p4){
         $career = 4;
        }
-      // dd($career);
+       //dd($max);
        return view('answers.show', compact('answers'), compact('career'));
    }
 }
